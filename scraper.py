302b1fbeee6ed343bbb8f7b17a0f79076d37fea1
Young Kim <young-k@protonmail.com>
2019-04-06 23:15:43 -0400

    My space bar is broken and it's clearly not infinite

--- scraper.py
+++ scraper.py
@@ -1,36 +1,35 @@
+import datetime
 import json
 import lxml
 import praw
 import requests
 from bs4 import BeautifulSoup
 
+data = []
+#after_id = 't3_b9msry'
+after_id = 't3_b9imfg'
+
 def serialize_comment(comment):
   curr_comment = {}
   curr_comment["author"] = comment.author.name
   curr_comment["author_reply"] = None
-  curr_comment["body"] = comment.body.encode('utf8')
+  curr_comment["body"] = comment.body
   curr_comment["created_utc"] = comment.created_utc
   curr_comment["score"] = comment.score
   return curr_comment
-
 
 def write_sample_to_file(num_posts, num_comments):
   reddit = praw.Reddit(client_id='wQaHdUWqKKgaLQ',
                        client_secret='mIDBYtZGLe_Pve7RiGcw2VWc-y4',
                        user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0')
 
-  subs = []
-  for submission in reddit.subreddit('depression').top('year', limit=num_posts):
-    subs.append(submission)
+  global after_id
+  params = {'after': after_id}
+  submissions = reddit.subreddit('depression').new(limit=num_posts, params=params)
 
-  ret = []
-  for i, submission in enumerate(subs):
-    if i % 2000 == 0:
-      f = open("year{}.txt".format(str(i)), "w+")
-      json.dump(ret, f)
-      f.close()
-      ret = []
-
+  for submission in submissions:
+    after_id = 't3_' + submission.id
+    print(after_id)
     if submission.author is None or submission.selftext_html is None:
       continue
 
@@ -42,12 +41,12 @@ def write_sample_to_file(num_posts, num_comments):
 
     curr_submission["author"] = submission.author.name
     curr_submission["created_utc"] = submission.created_utc
-    curr_submission["title"] = submission.title.encode('utf8')
+    curr_submission["id"] = submission.id
+    curr_submission["title"] = submission.title
     curr_submission["url"] = submission.url
     curr_submission["upvote_ratio"] = submission.upvote_ratio
 
-    print(i)
-    print(curr_submission["title"])
+    print(submission.title)
 
     submission.comment_sort = 'top'
     top_comments = submission.comments.list()
@@ -69,10 +68,22 @@ def write_sample_to_file(num_posts, num_comments):
           if reply.author.name == curr_submission["author"]:
             curr_comment["author_reply"] = serialize_comment(reply)
 
-    ret.append(curr_submission)
-  f = open("year{}.txt".format(str(i)), "w+")
-  json.dump(ret, f)
-  f.close()
+    data.append(curr_submission)
 
-if __name__ == "__main__":
-  write_sample_to_file(10, 30)
+if __name__ == '__main__':
+  #write_sample_to_file(None, 1, 40)
+  #after_id = 't3_' + data[-1]['id']
+
+  while True:
+    counter = 1
+
+    while len(data) < 200:
+      print(len(data))
+      print(after_id)
+      write_sample_to_file(1000, 40)
+
+    counter += 1
+    f = open('./infinite/{}.txt'.format(str(counter)), 'w+')
+    json.dump(data, f)
+    f.close()
+    data = []
